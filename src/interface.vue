46b44744a39da32952c85b145af8430fa805524a
FerrariAndrea <ferrari.andrea.95.b10@gmail.com>
2023-02-16 10:01:19 +0100

    is key an obj?

--- src/interface.vue
+++ src/interface.vue
@@ -107,9 +107,12 @@ export default {
     async function send(id,body) {
       toggleProgress()
       const url = `${apiUrl.value}/${folder}/${id}`;
+      const key = apiKey;
+      console.log("key",key);
+      console.log("key.value",key.value);
       const response = await axios.put(url, body, {headers:{  
         'content-type': 'application/octet-stream',
-        AccessKey: apiKey
+        AccessKey: key
       }});
       toggleProgress()
 
